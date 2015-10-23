529a255324b225b558dccf71e4ec734e9a837980
Vince Chan <vince.yp.chan@gmail.com>
2015-10-23 15:27:07 -0700

    Use postgresql as the MusicStore database
    The MVCMusicStore sample uses two databases.
    One is the music store which is converted to use postgresql
    the other one is the ASPNETDB, this is not converted

--- MvcMusicStore/Models/MusicStoreEntities.cs
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -10,5 +10,17 @@ namespace MvcMusicStore.Models
         public DbSet<Cart> Carts { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
-    }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+			// do not use migration
+			Database.SetInitializer<MusicStoreEntities>(null);
+
+			modelBuilder.Entity<Artist>().ToTable("Artist", "public");
+			modelBuilder.Entity<Album>().ToTable("Album", "public");
+			modelBuilder.Entity<Cart>().ToTable("Cart", "public");
+			modelBuilder.Entity<Order>().ToTable("Order", "public");
+			modelBuilder.Entity<OrderDetail>().ToTable("OrderDetail", "public");
+			modelBuilder.Entity<Genre>().ToTable("Genre", "public");
+		}
+	}
 }
